3791e763e17460f5a4d0f79dd72e072af9856437
Brenno Marques <brennoms.dev@gmail.com>
2026-05-01 22:55:18 -0300

    adiciona a classe Ponto

---
+++ src/core/Ponto.java
@@ -0,0 +1,41 @@
+package core;
+
+public class Ponto {
+
+	private double[] coordenadas = new double[3];
+
+	public Ponto(double x, double y, double z) {
+		reposicionar(x, y, z);
+	}
+
+	public double getX() { return coordenadas[0]; }
+	public double getY() { return coordenadas[1]; }
+	public double getZ() { return coordenadas[2]; }
+
+	public double[] getCoordenadas() {
+		return coordenadas;
+	}
+
+	public void reposicionar(double x, double y, double z) {
+		coordenadas[0] = x;
+		coordenadas[1] = y;
+		coordenadas[2] = z;
+	}
+
+	public void reposicionar(Ponto ponto) {
+		coordenadas[0] = ponto.getX();
+		coordenadas[1] = ponto.getY();
+		coordenadas[2] = ponto.getZ();
+	}
+
+	public void reposicionar(double[] coordenadas) {
+		this.coordenadas[0] = coordenadas[0];
+		this.coordenadas[1] = coordenadas[1];
+		this.coordenadas[2] = coordenadas[2];
+	}
+
+	public String toString() {
+		return String.format("Ponto( %.2f , %.2f , %.2f )", coordenadas[0], coordenadas[1], coordenadas[2]);
+	} 
+
+}
